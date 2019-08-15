b15e9b6f20c01b35d24b8598ae82b6f0eae86477
Gosick <goooosick@gmail.com>
2019-08-15 22:20:54 +0800

    chapter 7, diffuse material

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,24 @@ mod camera;
 
 /// convert Vec3 to RGB8([u8; 3])
 pub fn vec_to_rgb(c: Color) -> [u8; 3] {
+    // gamma correction
+    let c = Color::new(c.x.sqrt(), c.y.sqrt(), c.z.sqrt());
+    // convert to RGB 256
     let c = c.clamp(0.0, 1.0) * 255.99;
     [c[0] as u8, c[1] as u8, c[2] as u8]
+}
+
+/// generate random point in unit shpere
+pub fn random_in_unit_sphere() -> Vec3 {
+    use rand::prelude::*;
+
+    let mut rng = rand::thread_rng();
+    loop {
+        let p = Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), rng.gen::<f32>());
+        let p = 2.0 * p - Vec3::unit();
+
+        if p.norm_squared() <= 1.0 {
+            return p;
+        }
+    }
 }
